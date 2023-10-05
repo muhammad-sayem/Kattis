a4f90e3be0e5eaa7b9d97f582701993fb9ae1b28
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-10-06 00:10:34 +0600

    Electron Paradox

---
+++ Electron_Paradox.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             "YES"
+#define      no              "NO"
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    ll n, i, sum = 0;
+    cin >> n;
+    ll a[n+5];
+
+    for(i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    sort(a, a+n, greater<ll>());
+
+    ll val = n/2;
+
+    for(i=0; i<val; i++){
+        sum += a[i];
+    }
+
+    for(i=val; i<n; i++){
+        sum += a[i]/2;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
